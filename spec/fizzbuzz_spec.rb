d8c2d5c4c747004816e5bcaff62b6d0d8343eb2e
Ynda Jas <yndajas@gmail.com>
2020-06-25 03:45:05 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,5 @@
 require_relative './spec_helper.rb'
-require './fizzbuzz.rb'
+require '../fizzbuzz.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 describe "fizzbuzz" do
